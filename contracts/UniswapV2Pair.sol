bf1f7512dd63d5b539ef67fdaf908601087eb120
Yinbo Wen <ywen@ancilia.ai>
2024-02-02 10:43:36 -0800

    更新一些关于swap的注释

--- contracts/UniswapV2Pair.sol
+++ contracts/UniswapV2Pair.sol
@@ -41,8 +41,13 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
         _blockTimestampLast = blockTimestampLast;
     }
 
+    // 这里没有使用erc20的transfer方法，而是使用了safeTransfer方法
+    // 这个safe在可以根据返回结果来处理逻辑
+    // https://ethereum.stackexchange.com/questions/137882/why-does-uniswap-v2-use-safetransfer-to-transfer-tokens
     function _safeTransfer(address token, address to, uint value) private {
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(SELECTOR, to, value));
+        // 需要返回值是true，或者当data不为空的时候，decode按照bool解码之后也需要是true，否则就revert
+        // 通过这样的方式来保证只有在成功转账之后才会继续之后的代码
         require(success && (data.length == 0 || abi.decode(data, (bool))), 'UniswapV2: TRANSFER_FAILED');
     }
 
@@ -150,14 +155,21 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
         balance0 = IERC20(_token0).balanceOf(address(this));
         balance1 = IERC20(_token1).balanceOf(address(this));
 
+        // 更新预言机什么的
         _update(balance0, balance1, _reserve0, _reserve1);
         if (feeOn) kLast = uint(reserve0).mul(reserve1); // reserve0 and reserve1 are up-to-date
         emit Burn(msg.sender, amount0, amount1, to);
     }
 
     // this low-level function should be called from a contract which performs important safety checks
+    // uniswap核心的swap方法
+    // 前两个参数就是这个池子中的两种代币，每种代币需要转出多少token
+    // to是swap出来的代币要转到的地址
+    // data是为了闪电贷准备的
+    // lock是为了防止重入
     function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external lock {
         require(amount0Out > 0 || amount1Out > 0, 'UniswapV2: INSUFFICIENT_OUTPUT_AMOUNT');
+        // 因为两个reserve接下来总被使用，从节省gas的角度，可以先把取出来，放在内存中，每次从内存中读取比较节省gas
         (uint112 _reserve0, uint112 _reserve1,) = getReserves(); // gas savings
         require(amount0Out < _reserve0 && amount1Out < _reserve1, 'UniswapV2: INSUFFICIENT_LIQUIDITY');
 
@@ -166,19 +178,33 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
         { // scope for _token{0,1}, avoids stack too deep errors
         address _token0 = token0;
         address _token1 = token1;
+        //todo 这里不是特别明白为什么不允许to是两个token地址
+        // 讲道理转给token地址也没什么问题
         require(to != _token0 && to != _token1, 'UniswapV2: INVALID_TO');
+        // 要多少钱就给多少钱，先给到to地址
         if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
         if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
+        // 这个就是闪电贷的逻辑，to地址通过实现这个接口来实现闪电贷逻辑
         if (data.length > 0) IUniswapV2Callee(to).uniswapV2Call(msg.sender, amount0Out, amount1Out, data);
+        // 得到当前的池子合约中持有的两种token的数量
+        // 这里有一个比较有意思的问题，一般而言，balance中多出来的都是自己转进去的
+        // 但是如果有人通过transfer的方式往这个合约里面转token，有可能他是无意中用错了方法，导致balance变多了
+        // 这种情况需要考虑，这种情况，可以通过skim来把多出来的钱取走
         balance0 = IERC20(_token0).balanceOf(address(this));
         balance1 = IERC20(_token1).balanceOf(address(this));
         }
+        // 通过计算储备金和balance之间的差值，得到存进来的token，也就是amountIn
         uint amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0;
         uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
         require(amount0In > 0 || amount1In > 0, 'UniswapV2: INSUFFICIENT_INPUT_AMOUNT');
         { // scope for reserve{0,1}Adjusted, avoids stack too deep errors
+        // 在uniswap外围合约计算需要swapin的token数量的时候，也就是getAmountIn方法在计算的时候
+        // 就考虑到了fee的问题，所以在这里，先要减去千分之三的fee，然后用减去之后的数值去计算
+        // 这样的话，那么在每次swap之后，真个池子是多了一点点的钱，也就是手续费，这个fee会一直存在池子中，当做lp的奖励
         uint balance0Adjusted = balance0.mul(1000).sub(amount0In.mul(3));
         uint balance1Adjusted = balance1.mul(1000).sub(amount1In.mul(3));
+        // 无论怎么swap，到这一步，也就是uniswap的判断依据，就是无论怎么swap，最后的两种token（减去fee之后的）的数量的乘积，
+        // 也就是k，只要不小于原来的k值，即表示swap成功
         require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(1000**2), 'UniswapV2: K');
         }
 
